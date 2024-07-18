0a8d7b2e24c031a6cf906e68cd9178a945d4bced
jrphilo <james.philo@me.com>
2024-07-18 11:14:21 +0200

    feat: tweak handling of core statuses
    Signed-off-by: jrphilo <james.philo@me.com>

--- components/orders/order-statuses.tsx
+++ components/orders/order-statuses.tsx
@@ -1,13 +1,13 @@
 import { Order } from 'lib/shopify/types';
-import CoreReturnStatus from './core-return-status';
-import WarrantyActivatedStatus from './warranty-activated-status';
+import CoreStatus from './core-return-status';
 import OrderConfirmedStatus from './order-confirmation-status';
+import WarrantyActivatedStatus from './warranty-activated-status';
 
 export default function OrderStatuses({ order, className }: { order: Order; className?: string }) {
   return (
     <div className={className}>
       <OrderConfirmedStatus order={order} />
-      <CoreReturnStatus order={order} />
+      <CoreStatus order={order} />
       <WarrantyActivatedStatus order={order} />
     </div>
   );
