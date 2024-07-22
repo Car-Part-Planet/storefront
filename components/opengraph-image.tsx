5b6310cbde622d50391b255e374bda1ed23fef61
Chloe <pinkcloudvnn@gmail.com>
2024-07-22 10:21:38 +0700

    fix issue with og image
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -1,9 +1,27 @@
 import { ImageResponse } from 'next/og';
 import LogoSquare from './logo-square';
 
-export default async function OpengraphImage(): Promise<ImageResponse> {
-  return new ImageResponse(<LogoSquare />, {
-    width: 1200,
-    height: 630
-  });
+export type Props = {
+  title?: string;
+};
+
+export default async function OpengraphImage(props?: Props): Promise<ImageResponse> {
+  const { title } = {
+    ...{
+      title: process.env.SITE_NAME
+    },
+    ...props
+  };
+
+  return new ImageResponse(
+    (
+      <div tw="flex h-full w-full items-center justify-center bg-white">
+        <LogoSquare />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630
+    }
+  );
 }
