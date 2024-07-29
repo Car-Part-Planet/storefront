0cd7cb567efd8353f8483a3d2a340dc9a9e5bcdc
Chloe <pinkcloudvnn@gmail.com>
2024-07-29 18:56:05 +0700

    add PDP shema.org (#67)

--- app/(storefront)/product/[handle]/page.tsx
+++ app/(storefront)/product/[handle]/page.tsx
@@ -10,13 +10,16 @@ import { Gallery } from 'components/product/gallery';
 import { ProductDescription } from 'components/product/product-description';
 import { HIDDEN_PRODUCT_TAG } from 'lib/constants';
 import { getProduct, getProductRecommendations } from 'lib/shopify';
-import { Image } from 'lib/shopify/types';
+import { Image, Product } from 'lib/shopify/types';
+
+const { SITE_NAME, STORE_PREFIX } = process.env
 
 export async function generateMetadata({
   params
 }: {
   params: { handle: string };
-}): Promise<Metadata> {
+}): Promise<Metadata>
+{
   const product = await getProduct(params.handle);
 
   if (!product) return notFound();
@@ -37,15 +40,15 @@ export async function generateMetadata({
     },
     openGraph: url
       ? {
-          images: [
-            {
-              url,
-              width,
-              height,
-              alt
-            }
-          ]
-        }
+        images: [
+          {
+            url,
+            width,
+            height,
+            alt
+          }
+        ]
+      }
       : null
   };
 }
@@ -56,17 +59,40 @@ export default async function ProductPage({
 }: {
   params: { handle: string };
   searchParams?: { [key: string]: string | string[] | undefined };
-}) {
+})
+{
   const product = await getProduct(params.handle);
 
   if (!product) return notFound();
 
+  const shippingPrice = STORE_PREFIX === 'reman-transmission' ? '299.00' : '0.00';
+  const variant = product.variants.find((variant) =>
+    variant.selectedOptions.every(
+      (option) => option.value === searchParams?.[option.name.toLowerCase()]
+    )
+  );
+
+  const additionalProperties: Array<{ title: string, key: keyof Product, value: string | undefined }> = [
+    { key: 'transmissionType', title: 'Transmission Type', value: product.transmissionType?.toString() },
+    { key: 'transmissionSpeeds', title: 'Number of Speeds', value: `${product.transmissionSpeeds?.[0]}-Speed` },
+    { key: 'driveType', title: 'Drivetrain', value: product.driveType?.[0] },
+    { key: 'engineCylinders', title: 'Number of Cylinders', value: `${product.engineCylinders?.[0]} Cylinders` },
+    { key: 'transmissionCode', title: 'Transmission Code', value: product.transmissionCode?.[0] }
+  ];
+
+  const validProperties = additionalProperties.filter((property) => product[property.key])
   const productJsonLd = {
     '@context': 'https://schema.org',
     '@type': 'Product',
     name: product.title,
-    description: product.description,
+    description: product.description || product.seo.description,
     image: product.featuredImage?.url,
+    sku: variant?.sku?.substring(0, 6),
+    additionalProperty: validProperties.map(prop => ({
+      '@type': 'PropertyValue',
+      name: prop.title,
+      value: prop.value
+    })),
     offers: {
       '@type': 'AggregateOffer',
       availability: product.availableForSale
@@ -74,8 +100,31 @@ export default async function ProductPage({
         : 'https://schema.org/OutOfStock',
       priceCurrency: product.priceRange.minVariantPrice.currencyCode,
       highPrice: product.priceRange.maxVariantPrice.amount,
-      lowPrice: product.priceRange.minVariantPrice.amount
-    }
+      lowPrice: product.priceRange.minVariantPrice.amount,
+      itemCondition: 'https://schema.org/RefurbishedCondition',
+      seller: {
+        '@type': 'Organization',
+        name: SITE_NAME
+      },
+      url: `${process.env.SHOPIFY_ORIGIN_URL}/product/${product.handle}`,
+      shippingDetails: {
+        '@type': 'OfferShippingDetails',
+        shippingRate: {
+          '@type': 'MonetaryAmount',
+          currency: 'USD',
+          value: shippingPrice
+        }
+      },
+      warranty: {
+        '@type': 'WarrantyPromise',
+        durationOfWarranty: {
+          '@type': 'QuantitativeValue',
+          value: variant?.warranty_years || '3',
+          unitCode: 'ANN'
+        },
+        warrantyScope: 'https://schema.org/PartsAndLaborWarrantyScope'
+      }
+    },
   };
 
   return (
@@ -122,7 +171,8 @@ export default async function ProductPage({
   );
 }
 
-async function RelatedProducts({ id }: { id: string }) {
+async function RelatedProducts({ id }: { id: string })
+{
   const relatedProducts = await getProductRecommendations(id);
 
   if (!relatedProducts.length) return null;
