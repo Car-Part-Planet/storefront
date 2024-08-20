4e458aca60971298311dad36dbf1619e0d60d5f8
Chloe <pinkcloudvnn@gmail.com>
2024-08-20 23:05:15 +0700

    add used and remanufactured options
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- components/product/vairant-details.tsx
+++ components/product/vairant-details.tsx
@@ -1,22 +1,64 @@
 'use client';
 
 import Price from 'components/price';
-import { useProduct } from 'context/product-context';
+import { useProduct, useUpdateURL } from 'context/product-context';
 import { Product } from 'lib/shopify/types';
 import { findVariantWithMinPrice } from 'lib/utils';
-import { VariantSelector } from './variant-selector';
+import { useEffect, useMemo, useTransition } from 'react';
+import { Combination, VariantSelector } from './variant-selector';
 
 type VariantDetailsProps = {
   product: Product;
 };
 
 const VariantDetails = ({ product }: VariantDetailsProps) => {
-  const { variant } = useProduct();
+  const { variant, state, updateOptions } = useProduct();
 
   const variantWithMinPrice = findVariantWithMinPrice(product.variants);
+  const updateURL = useUpdateURL();
+  const [, startTransition] = useTransition();
 
   const price = variant?.price.amount || variantWithMinPrice?.price.amount || 0;
   const currencyCode = variant?.price.currencyCode || 'USD';
+  const variants = product.variants;
+
+  const combinations: Combination[] = useMemo(
+    () =>
+      variants.map((variant) => ({
+        id: variant.id,
+        availableForSale: variant.availableForSale,
+        ...variant.selectedOptions.reduce(
+          (accumulator, option) => ({ ...accumulator, [option.name.toLowerCase()]: option.value }),
+          {}
+        )
+      })),
+    [variants]
+  );
+
+  // If a variant is not selected, we want to select the first available for sale variant as default
+  useEffect(() => {
+    const hasSelectedVariant = Object.entries(state).some(([key, value]) => {
+      return combinations.some((combination) => combination[key] === value);
+    });
+
+    if (!hasSelectedVariant) {
+      const defaultVariant =
+        variantWithMinPrice || variants.find((variant) => variant.availableForSale);
+      if (defaultVariant) {
+        startTransition(() => {
+          const newState = updateOptions(
+            defaultVariant.selectedOptions.map((option) => ({
+              name: option.name.toLowerCase(),
+              value: option.value
+            }))
+          );
+
+          updateURL(newState);
+        });
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [variantWithMinPrice, variants, combinations]);
 
   return (
     <div className="mt-1">
@@ -25,11 +67,22 @@ const VariantDetails = ({ product }: VariantDetailsProps) => {
         currencyCode={currencyCode}
         className="text-2xl font-semibold text-emerald-500 md:text-4xl"
       />
-      <VariantSelector
-        options={product.options}
-        variants={product.variants}
-        minPrice={product.priceRange.minVariantPrice}
-      />
+      <div className="mb-5 mt-3 flex flex-wrap items-center gap-3">
+        <VariantSelector
+          options={product.options}
+          variants={product.variants.filter((v) => v.condition === 'Used')}
+          minPrice={product.priceRange.minVariantPrice}
+          condition="used"
+          combinations={combinations}
+        />
+        <VariantSelector
+          options={product.options}
+          variants={product.variants.filter((v) => v.condition === 'Remanufactured')}
+          minPrice={product.priceRange.minVariantPrice}
+          condition="remanufactured"
+          combinations={combinations}
+        />
+      </div>
     </div>
   );
 };
